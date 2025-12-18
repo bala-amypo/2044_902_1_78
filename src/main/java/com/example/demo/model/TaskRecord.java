1623e4535b8358ce00408e7903b1ddfc84f0c77d
bala-amypo <bala@amypo.in>
2025-12-18 15:57:22 +0000

    User commit

--- src/main/java/com/example/demo/model/TaskRecord.java
+++ src/main/java/com/example/demo/model/TaskRecord.java
@@ -1,27 +1,27 @@
-package com.example.demo.model;
+// package com.example.demo.model.TakeRecord;
 
-import jakarta.persistance.*;
-import java. time.LocalDateTime;
-@Entity
-public class TaskRecord {
+// import jakarta.persistance.*;
+// import java. time.LocalDateTime;
+// @Entity
+// public class TaskRecord {
 
-    @Id
-    @generatedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+//     @Id
+//     @generatedValue(strategy = GenerationType.IDENTITY)
+//     private Long id;
 
-    @Column(unique = true)
-    private String taskCode;
-    private String taskName;
-    private String requiredSkill;
-    private String requiredSkillLevel;
-    private String priority;
-    private String status;
-    private LocalDateTime createdAt;
+//     @Column(unique = true)
+//     private String taskCode;
+//     private String taskName;
+//     private String requiredSkill;
+//     private String requiredSkillLevel;
+//     private String priority;
+//     private String status;
+//     private LocalDateTime createdAt;
 
-    @Prepersist
-    void onCreate() {
-        createdAt = LocalDateTime.now();
-            status = "OPEN";
-    }
+//     @Prepersist
+//     void onCreate() {
+//         createdAt = LocalDateTime.now();
+//             status = "OPEN";
+//     }
 
-}
+// }
