2830d132699dd956d83b60356632a99f3cc947f4
bala-amypo <bala@amypo.in>
2025-12-18 15:15:22 +0000

    User commit

--- src/main/java/com/example/demo/model/TaskRecord.java
+++ src/main/java/com/example/demo/model/TaskRecord.java
@@ -1,3 +1,12 @@
+package com.example.demo.model;
+
+import jakarta.persistance.*;
+import java. time.LocalDateTime;
+@Entity
 public class TakeRecord {
+
+    @Id
+    @generatedValue(strategy = GenerationType.IDENTITY)
+    
 
 }
