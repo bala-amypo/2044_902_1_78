bc6f3b0a47ebe9715f3bf2cbd405c4122f1e349c
bala-amypo <bala@amypo.in>
2025-12-25 10:35:37 +0000

    User commit

--- src/main/java/com/example/demo/model/TaskAssignmentRecord.java
+++ src/main/java/com/example/demo/model/TaskAssignmentRecord.java
@@ -1,38 +1,17 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
-import java.time.LocalDateTime;
 
 @Entity
-public class TaskAssignmentRecord {
+public class TaskAssignment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private Long taskId;
-
     private Long volunteerId;
-
-    private LocalDateTime assignedAt;
-
-    private String status; // ACTIVE / COMPLETED / CANCELLED
-
-    private String notes;
-
-    @PrePersist
-    void onAssign() {
-        assignedAt = LocalDateTime.now();
-        status = "ACTIVE";
-    }
+    private String status; // ASSIGNED / COMPLETED
 
     // getters & setters
-    public void setTaskId(Long taskId) {
-    this.taskId = taskId;
-}
-
-public void setVolunteerId(Long volunteerId) {
-    this.volunteerId = volunteerId;
-}
-
 }
