3618c3b02ed83ab99266d8aa16b851497bc4334b
bala-amypo <bala@amypo.in>
2025-12-26 17:34:05 +0000

    User commit

--- src/main/java/com/example/demo/repository/AssignmentEvaluationRecordRepository.java
+++ src/main/java/com/example/demo/repository/AssignmentEvaluationRecordRepository.java
@@ -2,8 +2,11 @@ package com.example.demo.repository;
 
 import com.example.demo.model.AssignmentEvaluationRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
+@Repository
 public interface AssignmentEvaluationRecordRepository extends JpaRepository<AssignmentEvaluationRecord, Long> {
     List<AssignmentEvaluationRecord> findByAssignmentId(Long assignmentId);
+    List<AssignmentEvaluationRecord> findByAssignment_Volunteer_Id(Long volunteerId);
 }
--- src/main/java/com/example/demo/repository/TaskRecordRepository.java
+++ src/main/java/com/example/demo/repository/TaskRecordRepository.java
@@ -2,10 +2,13 @@ package com.example.demo.repository;
 
 import com.example.demo.model.TaskRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface TaskRecordRepository extends JpaRepository<TaskRecord, Long> {
     List<TaskRecord> findByStatus(String status);
+    List<TaskRecord> findByRequiredSkill(String skillName);
     Optional<TaskRecord> findByTaskCode(String taskCode);
 }
