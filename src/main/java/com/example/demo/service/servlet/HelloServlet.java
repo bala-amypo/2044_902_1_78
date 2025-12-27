aa081d12b8fec9fec2eaab2462318f8938794743
bala-amypo <bala@amypo.in>
2025-12-27 04:23:20 +0000

    User commit

--- src/main/java/com/example/demo/service/servlet/HelloServlet.java
+++ src/main/java/com/example/demo/service/servlet/HelloServlet.java
@@ -9,7 +9,7 @@ import java.io.PrintWriter;
 public class HelloServlet extends HttpServlet {
     
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/plain");
         PrintWriter writer = resp.getWriter();
         writer.write("Hello from HelloServlet");
